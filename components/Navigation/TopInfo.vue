be1db47599596caff73f70c2f0955f0bacfedea1
lamkostal <lamkostal@gmail.com>
2019-03-25 17:31:04 +0200

    top-info mobile font-size

--- components/Navigation/TopInfo.vue
+++ components/Navigation/TopInfo.vue
@@ -2,11 +2,11 @@
     <section class="top-info">
         <div class="top-info-wrap container-fluid ">
                     
-                    <span class="topInfo-items is-size-7" >
+                    <span class="topInfo-items is-size-7 is-size-8-mobile" >
                     <a href="mailto:&#032;&#103;&#106;&#097;&#110;&#097;&#115;&#116;&#111;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;" id="top-info-email" class="icon-links"><i class="fas fa-envelope is-size-6"></i></a>
                         gjanasto@gmail[τελεία]com
                     </span>
-                    <span class="topInfo-items is-size-7" >
+                    <span class="topInfo-items is-size-7 is-size-8-mobile" >
                        <a href="tel:2109955925" id="top-info-tel" class="icon-links"><i class="fas fa-phone is-size-6"></i></a>
                         210 9955925</span>
         </div>
