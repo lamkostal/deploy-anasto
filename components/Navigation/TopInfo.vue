287391fcab7472b75d8da5ff57fa2c4fd64dbea2
lamkostal <lamkostal@gmail.com>
2019-04-04 12:24:40 +0300

    share icons, obfuscate

--- components/Navigation/TopInfo.vue
+++ components/Navigation/TopInfo.vue
@@ -1,47 +1,53 @@
 <template>
-    <section class="top-info">
-        <div class="top-info-wrap container-fluid ">
-                    
-                    <span class="topInfo-items is-size-7 is-size-8-mobile" >
-                    <a href="mailto:&#032;&#103;&#106;&#097;&#110;&#097;&#115;&#116;&#111;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;" id="top-info-email" class="icon-links"><i class="fas fa-envelope is-size-6"></i></a>
-                        gjanasto@gmail[τελεία]com
-                    </span>
-                    <span class="topInfo-items is-size-7 is-size-8-mobile" >
-                       <a href="tel:2109955925" id="top-info-tel" class="icon-links"><i class="fas fa-phone is-size-6"></i></a>
-                        210 9955925</span>
-        </div>
-    </section>
+  <section class="top-info">
+    <div class="top-info-wrap container-fluid">
+      <span class="topInfo-items is-size-7 is-size-8-mobile">
+        <a
+          href="&#109;ailto&#58;%6&#55;&#106;an%&#54;1st%6&#70;&#64;g&#37;&#54;D%61%69l&#46;&#37;63o&#109;"
+          id="top-info-email"
+          class="icon-links"
+        >
+          <i class="fas fa-envelope is-size-6"></i>
+        </a>
+        gjanasto@gmail[τελεία]com
+      </span>
+      <span class="topInfo-items is-size-7 is-size-8-mobile">
+        <a href="tel:2109955925" id="top-info-tel" class="icon-links">
+          <i class="fas fa-phone is-size-6"></i>
+        </a>
+        210 9955925
+      </span>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-    name:"TopInfo"
-}
+  name: "TopInfo"
+};
 </script>
 
 <style scoped>
-    .top-info{
-         background-color: #DBAC4D
-         
-    }
-    .top-info-wrap{
-        height:auto;
-        display: flex;
-        justify-content: flex-end;
-        padding:0 10px;
-    }
-    .topInfo-items{
-        padding: 5px 5px;
-        color:#fff;
-    }
-    .icon-links{
-        color: #fff;
-        padding:0px 5px;
-    }
-    .icon-links:hover{
-        color:#197070;
-    }
-    
+.top-info {
+  background-color: #dbac4d;
+}
+.top-info-wrap {
+  height: auto;
+  display: flex;
+  justify-content: flex-end;
+  padding: 0 10px;
+}
+.topInfo-items {
+  padding: 5px 5px;
+  color: #fff;
+}
+.icon-links {
+  color: #fff;
+  padding: 0px 5px;
+}
+.icon-links:hover {
+  color: #197070;
+}
 </style>
 
 
