d9dc86f1e604985c66450cce8287f39fbf5e3f8a
lamkostal <lamkostal@gmail.com>
2019-04-08 20:34:42 +0300

    nuxt/robots

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,7 +48,8 @@ module.exports = {
   modules: [
     // Doc:https://github.com/nuxt-community/modules/tree/master/packages/bulma
     '@nuxtjs/bulma',
-    '@nuxtjs/sitemap'
+    '@nuxtjs/sitemap',
+    '@nuxtjs/robots'
   ],
 
   /*
