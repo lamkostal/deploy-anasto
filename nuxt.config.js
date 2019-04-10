f15c7dd11e30c82b2b023c1d4bee4d02ec1dbfd8
lamkostal <lamkostal@gmail.com>
2019-04-10 19:22:14 +0300

    now?

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,7 @@ module.exports = {
       { rel: 'stylesheet', 
         href: 'https://use.fontawesome.com/releases/v5.7.2/css/all.css',
         integrity : "sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr",
-        crossorigin : "anonymous",
+        crossorigin : "anonymous"
        },
        {href:"https://fonts.googleapis.com/css?family=Roboto" ,rel:"stylesheet"}
     ]
